aa5ae18172216370886c9b62cac21942ec6a9353
loster12520 <xiaoshuo12520@outlook.com>
2024-03-11 01:22:01 +0800

    把BlankOrder.kt文件给更新了一下，方便后续的编写代码 2024-03-11-01-21

--- src/main/kotlin/com/lignting/orders/BlankOrder.kt
+++ src/main/kotlin/com/lignting/orders/BlankOrder.kt
@@ -5,10 +5,26 @@ import com.lignting.main.OrderLoader
 class BlankOrder(
     override val orderPrefix: String,
     override val orderText: String,
-    val function: (List<String>, OrderLoader) -> String
+    val function: (List<String>, OrderLoader) -> String,
+    override val description:String,
+    override val author:String,
+    override val version:String,
+    override val details:String
 ) : AbstractOrder() {
     override fun doIt(orderArgs: List<String>, orderLoader: OrderLoader): String {
         return function(orderArgs, orderLoader)
     }
+
+    constructor(
+        orderPrefix: String,
+        orderText: String,
+        function: (List<String>, OrderLoader) -> String
+    ):this(
+        orderPrefix,orderText,function,
+        "一个通过空白指令创建的初始指令",
+        "unknown",
+        "unknown",
+        "unknown"
+    )
 
 }
